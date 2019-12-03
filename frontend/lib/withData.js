988b3218663dfa1d1193ea6006154228f4a4480e
Benny Wang <xjw@umich.edu>
2019-12-03 06:04:39 -0800

    cors revision

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -9,13 +9,12 @@ function createClient({ headers }) {
   return new ApolloClient({
     uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
     request: operation => {
-      // operation.setContext({
-      //   fetchOptions: {
-      //     credentials: 'include',
-      //   },
+      operation.setContext({
+        fetchOptions: {
+          credentials: 'include',
+        },
         headers,
-      }
-      // );
+      });
     },
     //local data
     clientState: {
