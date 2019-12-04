786c1ea33940c4fa0c3b940b41928c0ffa4393cb
Benny Wang <xjw@umich.edu>
2019-12-03 21:30:18 -0800

    apolloURI

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -7,7 +7,7 @@ import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+    uri: apolloUri,
     request: operation => {
       operation.setContext({
         fetchOptions: {
