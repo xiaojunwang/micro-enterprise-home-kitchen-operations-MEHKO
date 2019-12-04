83d5895620987010695b7cffdd4ba1bd378c7bd6
Benny Wang <xjw@umich.edu>
2019-12-03 20:36:02 -0800

    prodEndpoint adjust

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fd06c22ff60dfc7bf4c1";
+/******/ 	var hotCurrentHash = "688c03c5996614141dfe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
